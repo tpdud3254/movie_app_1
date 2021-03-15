d4be74b0329d7a58f1fcc7dbb3874790eba2c34e
ksy <tpdud3256@gmail.com>
2021-03-15 22:54:54 +0900

    Updates

---
+++ precache-manifest.ab8a34bc6872a7d08e3da7453052df3b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b129a56801402ad90403",
+    "url": "/movie_app_2019/static/css/main.b1df4fc7.chunk.css"
+  },
+  {
+    "revision": "b129a56801402ad90403",
+    "url": "/movie_app_2019/static/js/main.10b5e059.chunk.js"
+  },
+  {
+    "revision": "5a2b9365f7ca01d151d6",
+    "url": "/movie_app_2019/static/js/runtime~main.64ac130e.js"
+  },
+  {
+    "revision": "5506dd43cdbf65441d98",
+    "url": "/movie_app_2019/static/js/2.d07a8bac.chunk.js"
+  },
+  {
+    "revision": "d32ce3b90617a90e572bd387417c5d2d",
+    "url": "/movie_app_2019/index.html"
+  }
+];
